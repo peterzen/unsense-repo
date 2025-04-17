af5d0968ed683b467170a39190d0e48eabd4b176
Peter Banik <peter@froggle.org>
2025-04-17 18:59:56 +0000

    Clickable row in tables

--- src/unsense-theme/src/javascripts/theme.js
+++ src/unsense-theme/src/javascripts/theme.js
@@ -1,30 +1,41 @@
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-  // wrap checkboxes with labels
+  // wrap checkboxes with labels to enlarge clickable area
   $('[type="checkbox"]').each(function () {
     const $cb = $(this);
     const parent = this.parentNode;
-  
+
     const textNode = Array.from(parent.childNodes).find(
       n => n.nodeType === Node.TEXT_NODE && n.textContent.trim().length > 0
     );
-  
+
     if (textNode) {
       $cb.add(textNode).wrapAll('<label class="control-label legacy-label"></label>');
     }
   });
 
   // toggle selectable rows on row click in bootgrid tables
-  $('.bootgrid-table > tbody > tr').each(function(){
+  $('.bootgrid-table > tbody > tr').each(function () {
     const $cb = $(this).find('td.select-cell input[type="checkbox"]');
-  
-    $(this).on('click', function(e){
+
+    $(this).on('click', function (e) {
       if (e.target !== $cb[0]) {
         $cb.prop('checked', !$cb.prop('checked')).trigger('change');
       }
     });
   });
 
+  // toggle selectable rows on row click in bootgrid tables
+  $('table.opnsense-rules > tbody > tr').each(function () {
+    const $cb = $(this).find('.rule_select');
+
+    $(this).on('click', function (e) {
+      if (e.target !== $cb[0]) {
+        $cb.prop('checked', !$cb.prop('checked')).trigger('change');
+      }
+    });
+    $(this).addClass('selectable-row');
+  });
 });
 
