9a4d63149d31f9e2e39293a6c9ead1d41266ad5a
Peter Banik <peter@froggle.org>
2025-04-17 11:19:46 +0000

    Add row click and checkbox label behaviors

--- src/unsense-theme/scripts/watch.sh
+++ src/unsense-theme/scripts/watch.sh
@@ -6,7 +6,7 @@ DEST=build
 SASS_WARN_DEPRECATED=false
 INCLUDE_PATH=node_modules/bootstrap-sass/assets/stylesheets
 
-chokidar "$SRC/stylesheets/**/*" \
+chokidar "$SRC/stylesheets/**/*" "$src/javascripts/*" \
     -c "yarn build:dev" \
     --initial \
     --verbose
--- src/unsense-theme/src/javascripts/theme.js
+++ src/unsense-theme/src/javascripts/theme.js
@@ -1,42 +1,30 @@
-// document.addEventListener('readystatechange', (event) => {
-// 	document.styleSheets[4].disabled = true;
-// 	document.documentElement.setAttribute('data-theme', getTheme());
-// });
 
-// $(document).ready(function() {
+$(document).ready(function() {
 
-// 	$('.fa-building-o').addClass('fa-microchip').removeClass('fa-building-o');
-// 	$('.fa-map-signs').addClass('fa-network-wired').removeClass('fa-map-signs');
-// 	$('.fa-map').addClass('fa-chart-network').removeClass('fa-map');
-// 	$('.fa-expand').addClass('fa-network-wired').removeClass('fa-expand');
-// 	$('.fa-lock').addClass('fa-shield-check').removeClass('fa-lock');
-// 	$('.fa-clock-o').addClass('fa-clock').removeClass('fa-clocl-o');
-// 	$('.fa-refresh').addClass('fa-redo').removeClass('fa-refresh');
-// 	$('.glyphicon.glyphicon-fire').addClass('fa fa-route-highway').removeClass('glyphicon glyphicon-fire');
-// 	$('.fa-support').addClass('fa fa-question-circle').removeClass('fa-support');
-// 	$('.fa').addClass('fal fa-lg').removeClass('fa');
-
-// 	$('ul.nav.navbar-nav.navbar-right').append("<li><span class=\"navbar-theme\" id=\"theme_toggle\"><label class=\"theme-switch\" for=\"checkbox\"><input type=\"checkbox\" id=\"checkbox\" /><div class=\"slider round\"></div></label></span>")
-// 	const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-// 	toggleSwitch.addEventListener('input', switchTheme);
-// 	if (getTheme() === 'dark') {toggleSwitch.checked = true;}
-// });
-
-// function getTheme() {
-// 	if (localStorage.getItem('theme')) {theme = localStorage.getItem('theme');}
-// 	else theme = (window.matchMedia('(prefers-color-scheme: dark)').matches) ? 'dark' : 'light';
-// 	return theme;
-// }
-
-// function switchTheme(e) {
-// 	if (e.target.checked) {
-// 		document.documentElement.setAttribute('data-theme', 'dark');
-// 		localStorage.setItem('theme', 'dark');
-// 	}
-// 	else {
-// 		document.documentElement.setAttribute('data-theme', 'light');
-// 		localStorage.setItem('theme', 'light');
-// 	}
-//   };
-
+  // wrap checkboxes with labels
+  $('[type="checkbox"]').each(function () {
+    const $cb = $(this);
+    const parent = this.parentNode;
   
+    const textNode = Array.from(parent.childNodes).find(
+      n => n.nodeType === Node.TEXT_NODE && n.textContent.trim().length > 0
+    );
+  
+    if (textNode) {
+      $cb.add(textNode).wrapAll('<label class="control-label legacy-label"></label>');
+    }
+  });
+
+  // toggle selectable rows on row click in bootgrid tables
+  $('.bootgrid-table > tbody > tr').each(function(){
+    const $cb = $(this).find('td.select-cell input[type="checkbox"]');
+  
+    $(this).on('click', function(e){
+      if (e.target !== $cb[0]) {
+        $cb.prop('checked', !$cb.prop('checked')).trigger('change');
+      }
+    });
+  });
+
+});
+
